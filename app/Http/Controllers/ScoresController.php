fee7630f6cddf086f322bbb9c64b2b866097a210
stevepbrown <8r0Wn5t3r@gmail.com>
2017-10-05 15:49:55 +0100

    returning scores wit users

--- app/Http/Controllers/ScoresController.php
+++ app/Http/Controllers/ScoresController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use \App\models;
+
 
 class ScoresController extends Controller
 {
@@ -24,7 +25,28 @@ class ScoresController extends Controller
 
     $data = $request->all(); // This will get all the request data.
 
-    return '<p style="font-size:25px;font-color:green" class="alert alert-success">AJAX call has succeeded, server data available!</p>';
+//TODO - Remove this example $users = App\User::where('active', 1)->get();
+  //  $scores = models\Score::where('Score->user->name', $data['filterSelection'])->get();
+$scores = models\Score::with('user')->get();
 
+dd($score);
+
+
+// TODO dd($scores->user()->where('name', 'Estevan Johns')->get());
+
+
+    // switch($data->filterType){
+    //
+    //   case 'name':
+    //   $scores = Score::where('score->user->name', $filterSelection)->get();
+    //   case 'difficulty':
+    //     $scores = Score::where('score->difficulty', $filterSelection)->get();
+    //     default:
+    //     $scores = Score::all()-get();
+    //
+    //   }
+
+      return $scores;
+    }
+    //TODO Remove this $scores = App\Score::where('active', 1)->get();
   }
-}
